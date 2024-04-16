dacccd3f5f73ad005bbf4d697bbe3e95836c423c
Mohit Singh <unignoramus@gmail.com>
2024-04-16 17:08:22 +0530

    Add flame sensor to node

--- node/final.cpp
+++ node/final.cpp
@@ -10,6 +10,9 @@
 #define WiFi_SSID "SSID"
 #define WiFi_PASS "PASSWORD"
 
+#define FLAME_SEN_DIGITAL_PIN 5
+#define FLAME_SEN_ANALOG_PIN 4
+
 #define SMOKE_SEN_DIGITAL_PIN 2
 #define SMOKE_SEN_ANALOG_PIN 35
 
@@ -98,8 +101,22 @@ void error()
     }
 }
 
+void fireDetected()
+{
+    for (int i = 0; i < 256; i++)
+    {
+        analogWrite(LED_R, i);
+        analogWrite(LED_G, 0);
+        analogWrite(LED_B, 0);
+        delay(5);
+    }
+}
+
 void initializePins()
 {
+    pinMode(FLAME_SEN_DIGITAL_PIN, INPUT);
+    pinMode(FLAME_SEN_ANALOG_PIN, INPUT);
+
     pinMode(SMOKE_SEN_DIGITAL_PIN, INPUT);
     pinMode(SMOKE_SEN_ANALOG_PIN, INPUT);
 
@@ -168,6 +185,7 @@ float temperatureThreshold = 50;
 float heatIndexThreshold = 55;
 int humidityChangeThreshold = 20;
 int smokeAnalogThreshold = 1000;
+float flameThreshold = 0.5;
 
 void loop()
 {
@@ -211,6 +229,15 @@ void loop()
         Serial.print("Smoke Analog: ");
         Serial.println(smokeAnalogReading);
 
+        int flameDigitalReading = digitalRead(FLAME_SEN_DIGITAL_PIN);
+        float flameAnalogReading = analogRead(FLAME_SEN_ANALOG_PIN);
+
+        Serial.print("Flame Digital: ");
+        Serial.print(flameDigitalReading);
+        Serial.print("\t");
+        Serial.print("Flame Analog: ");
+        Serial.println(flameAnalogReading);
+
         // fetch the thresholds from thingspeak server
         int status_code = ThingSpeak.readMultipleFields(thresholdChannelNumber, thresholdReadAPIKey);
         if (status_code == 200)
@@ -224,7 +251,8 @@ void loop()
                 float tempHeatIndexThreshold = ThingSpeak.readStringField(thresholdChannelNumber, 2, thresholdReadAPIKey).toFloat();
                 int tempHumidityChangeThreshold = ThingSpeak.readStringField(thresholdChannelNumber, 3, thresholdReadAPIKey).toInt();
                 int tempSmokeAnalogThreshold = ThingSpeak.readStringField(thresholdChannelNumber, 4, thresholdReadAPIKey).toInt();
-                if (!tempTemperatureThreshold || !tempHeatIndexThreshold || !tempHumidityChangeThreshold || !tempSmokeAnalogThreshold)
+                float tempFlameThreshold = ThingSpeak.readStringField(thresholdChannelNumber, 6, thresholdReadAPIKey).toFloat();
+                if (!tempTemperatureThreshold || !tempHeatIndexThreshold || !tempHumidityChangeThreshold || !tempSmokeAnalogThreshold || !tempFlameThreshold)
                     Serial.println("Invalid threshold value(s) fetched, skipping update.");
                 else
                 {
@@ -232,6 +260,7 @@ void loop()
                     heatIndexThreshold = tempHeatIndexThreshold;
                     humidityChangeThreshold = tempHumidityChangeThreshold;
                     smokeAnalogThreshold = tempSmokeAnalogThreshold;
+                    flameThreshold = tempFlameThreshold;
                     Serial.println();
                     Serial.print("Temperature threshold: ");
                     Serial.println(temperatureThreshold);
@@ -241,6 +270,8 @@ void loop()
                     Serial.println(humidityChangeThreshold);
                     Serial.print("Smoke threshold: ");
                     Serial.println(smokeAnalogThreshold);
+                    Serial.print("Flame threshold: ");
+                    Serial.println(flameThreshold);
                     Serial.println();
                 }
             }
@@ -257,18 +288,19 @@ void loop()
             Serial.println("Error reading ThingSpeak channel");
         }
 
+        bool fire = false;
+
         ThingSpeak.setField(1, temperatureReadingCelsius);
         ThingSpeak.setField(2, heatIndexCelsius);
         ThingSpeak.setField(3, humidityReading);
         ThingSpeak.setField(4, smokeDigitalReading);
         ThingSpeak.setField(5, smokeAnalogReading);
+        ThingSpeak.setField(7, flameAnalogReading);
+        ThingSpeak.setField(8, flameDigitalReading);
 
-        if (temperatureReadingCelsius > temperatureThreshold ||
-            heatIndexCelsius > heatIndexThreshold ||
-            (previousHumidityReading != -1 &&
-             abs(previousHumidityReading - humidityReading) > humidityChangeThreshold) ||
-            smokeAnalogReading > smokeAnalogThreshold)
+        if (temperatureReadingCelsius > temperatureThreshold || heatIndexCelsius > heatIndexThreshold || (previousHumidityReading != -1 && abs(previousHumidityReading - humidityReading) > humidityChangeThreshold) || smokeAnalogReading > smokeAnalogThreshold || flameAnalogReading > flameThreshold)
         {
+            fire = true;
             digitalWrite(BUZZER_PIN, LOW);
             ThingSpeak.setField(6, 1);
         }
@@ -284,7 +316,10 @@ void loop()
         if (code == 200)
         {
             Serial.println("Channel update successful.");
-            success();
+            if (fire)
+                fireDetected();
+            else
+                success();
         }
         else
         {
